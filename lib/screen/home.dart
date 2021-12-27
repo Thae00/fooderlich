2ab44ad8e6ab44768ceeddd8977761e8b69cb291
Thae00 <mathaeoo97@gmail.com>
2021-12-27 13:01:54 +0630

    selectedIndex change view

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -8,6 +8,27 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   // Add state variables and functions
+  int _selectedIndex = 0;
+  static List<Widget> pages = [
+    //Replace with Card
+    Container(
+      color: Colors.red,
+    ),
+    //Replace with Card2
+    Container(
+      color: Colors.green,
+    ),
+    //Replace with Card3
+    Container(
+      color: Colors.blue,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,14 +43,13 @@ class _HomeState extends State<Home> {
         ),
       ),
       // Show selected tab
-      body: Center(
-          child: Text('Let\'s get cooking 👩‍🍳',
-              // 3
-              style: Theme.of(context).textTheme.headline1)),
+      body: pages[_selectedIndex],
       // bottom navigation bar
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
         // Set selected tab bar
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
         items: [
           BottomNavigationBarItem(
               icon: Icon(Icons.card_giftcard), label: 'Card'),
