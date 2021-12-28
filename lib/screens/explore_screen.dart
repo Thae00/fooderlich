ca67793f7e6732072562d6ab897f46109fceef32
Thae00 <mathaeoo97@gmail.com>
2021-12-28 20:19:43 +0630

    add scroll listener in explore screen

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -4,9 +4,40 @@ import '../components/components.dart';
 import '../models/models.dart';
 import '../api/mock_fooderlich_service.dart';
 
-class ExploreScreen extends StatelessWidget {
+class ExploreScreen extends StatefulWidget {
   // 1
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
   final mockService = MockFooderlichService();
+
+  late ScrollController _controller;
+
+  @override
+  void initState() {
+    _controller = ScrollController();
+    _controller.addListener(scrollListener);
+    super.initState();
+  }
+
+  void scrollListener() {
+    if (_controller.offset >= _controller.position.maxScrollExtent &&
+        !_controller.position.outOfRange) {
+      print('reached the bottom');
+    }
+    if (_controller.offset <= _controller.position.minScrollExtent &&
+        !_controller.position.outOfRange) {
+      print('reached the top!');
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(scrollListener);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +49,7 @@ class ExploreScreen extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.done) {
           // Add nested list Views
           return ListView(
+            controller: _controller,
             scrollDirection: Axis.vertical,
             children: [
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
