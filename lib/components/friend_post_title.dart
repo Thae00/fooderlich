b3a9e81b5633358b15eb3bb50e7d0aaba7fd512c
Thae00 <mathaeoo97@gmail.com>
2021-12-28 16:08:13 +0630

    creating friendPostTitle

---
+++ lib/components/friend_post_title.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/components/circle_image.dart';
+import 'package:fooderlich/models/models.dart';
+
+class FriendPostTitle extends StatelessWidget {
+  final Post post;
+  FriendPostTitle({required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CircleImage(
+          imageProvider: AssetImage(post.profileImageUrl),
+          imageRadius: 20,
+        ),
+        SizedBox(
+          width: 16,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '${post.timestamp} mins ago',
+                style: TextStyle(fontWeight: FontWeight.w700),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
