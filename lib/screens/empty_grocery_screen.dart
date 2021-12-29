b64bf24e5b91217af02c747c4dbfbb52cc1a87ac
Thae00 <mathaeoo97@gmail.com>
2021-12-29 21:23:34 +0630

    custom ui for emptyGroceryScreen

---
+++ lib/screens/empty_grocery_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class EmptyGroceryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Replace and add layout widgets
+    return Padding(
+      padding: EdgeInsets.all(30.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Add empty image
+            Flexible(
+              child: AspectRatio(
+                aspectRatio: 1 / 1,
+                child: Image.asset("assets/sample_pics/empty_list.png"),
+              ),
+            ),
+            // Add empty screen title
+            Text(
+              'No Groceries',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            // Add empty screen subtitleconst SizedBox(height: 16.0),
+            Text(
+              'Shopping for ingredients?\n'
+              'Tap the + button to write them down!',
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            // Add browse recipes button
+            MaterialButton(
+              onPressed: () {
+                // Go to Recipes Tab
+              },
+              child: Text(
+                'Browse Recipes',
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              textColor: Colors.white,
+              color: Colors.green,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/screens/empty_grocery_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Add a scaffold widget
+    
     // Replace with EmptyGroceryScreen
-    return Container(
-      color: Colors.green,
-    );
+    return EmptyGroceryScreen();
   }
 }
