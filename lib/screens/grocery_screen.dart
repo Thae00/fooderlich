704af373008ab66a3a53a8b509d14036da49a6df
Thae00 <mathaeoo97@gmail.com>
2021-12-30 14:52:59 +0630

    createing custom ui groceryScreen

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -5,26 +5,33 @@ import 'package:provider/provider.dart';
 
 class GroceryScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {    
-    //  Add a scaffold widget
-    return EmptyGroceryScreen();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //Present GroceryItemScreen
+        },
+        child: Icon(Icons.add),
+      ),
+      body: buildGroceryScreen(),
+    );
   }
+
   // Add buildGroceryScreen
   Widget buildGroceryScreen() {
     // 1
     return Consumer<GroceryManager>(
       // 2
       builder: (context, manager, child) {
-      // 3
-      if (manager.groceryItems.isNotEmpty) {
-        // TODO 25: Add GroceryListScreen
-        return Container();
-      } else {
-        // 4
-        return EmptyGroceryScreen();
-      }
-    },
+        // 3
+        if (manager.groceryItems.isNotEmpty) {
+          // TODO 25: Add GroceryListScreen
+          return Container();
+        } else {
+          // 4
+          return EmptyGroceryScreen();
+        }
+      },
     );
   }
-
 }
