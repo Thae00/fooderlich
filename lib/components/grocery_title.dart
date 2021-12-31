1d53a7146d00a74102b6fd3b674a21844d282ffd
Thae00 <mathaeoo97@gmail.com>
2021-12-31 15:58:28 +0630

    Displaying the importance label & RowToGroup

--- lib/components/grocery_title.dart
+++ lib/components/grocery_title.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/models/grocery_item.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class GroceryTitle extends StatelessWidget {
   final GroceryItem item;
@@ -14,13 +16,91 @@ class GroceryTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Change this widget
-    return Container(
-      height: 100,
-      // Replace this color
-      color: Colors.red,
+    return SizedBox(
+        height: 100,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Add Row to group (name, date, importance)
+            Row(
+              children: [
+                Container(
+                  width: 5,
+                  color: item.color,
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      item.name,
+                      style: GoogleFonts.lato(
+                        decoration: textDecoration,
+                        fontSize: 21,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    buildDate(),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    buildImportance(),
+                  ],
+                ),
+              ],
+            ),
+            // Add Row to group(quantity, checkbox)
+          ],
+        ));
+  }
+
+  //Add BuildImportance()
+  Widget buildImportance() {
+    if (item.importance == Importance.low) {
+      return Text(
+        'Low',
+        style: GoogleFonts.lato(decoration: textDecoration),
+      );
+    } else if (item.importance == Importance.medium) {
+      return Text(
+        'Medium',
+        style: GoogleFonts.lato(
+            fontWeight: FontWeight.w800, decoration: textDecoration),
+      );
+    } else if (item.importance == Importance.high) {
+      return Text(
+        'High',
+        style: GoogleFonts.lato(
+            color: Colors.red,
+            fontWeight: FontWeight.w900,
+            decoration: textDecoration),
+      );
+    } else {
+      throw Exception('This importance type does not exist');
+    }
+  }
+
+  //Add buildDate()
+  Widget buildDate() {
+    final dateFormatter = DateFormat('MMMM dd h:mm a');
+    final dateString = dateFormatter.format(item.date);
+    return Text(
+      dateString,
+      style: TextStyle(decoration: textDecoration),
     );
   }
-  //Add BuildImportance()
-  //Add buildDate()
   //Add buildCheckbox()
+
+  Widget buildCheckbox() {
+    return Checkbox(
+      value: item.isComplete,
+      onChanged: onComplete,
+    );
+  }
 }
