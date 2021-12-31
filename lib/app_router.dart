8684cd576ed4c77c755a05bc1d05b47250621d1c
Thae00 <mathaeoo97@gmail.com>
2021-12-31 21:38:39 +0630

    creating app router

---
+++ lib/app_router.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/models/models.dart';
+
+class AppRouter extends RouterDelegate
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
+  final AppStateManager appStateManager;
+  final GroceryManager groceryManager;
+  final ProfileManager profileManager;
+
+  AppRouter({
+    required this.appStateManager,
+    required this.groceryManager,
+    required this.profileManager,
+  }) : navigatorKey = GlobalKey<NavigatorState>() {
+    //  Add Listeners
+    appStateManager.addListener(notifyListeners);
+    groceryManager.addListener(notifyListeners);
+    profileManager.addListener(notifyListeners);
+  }
+  // Dispose listeners
+  @override
+  void dispose() {
+    appStateManager.removeListener(notifyListeners);
+    groceryManager.removeListener(notifyListeners);
+    profileManager.removeListener(notifyListeners);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      // Add onPopPage
+      onPopPage: _handlePopPage,
+      // 9
+      pages: [
+        // TODO: Add SplashScreen
+        // TODO: Add LoginScreen
+        // TODO: Add OnboardingScreen
+        // TODO: Add Home
+        // TODO: Create new item
+        // TODO: Select GroceryItemScreen
+        // TODO: Add Profile Screen
+        // TODO: Add WebView Screen
+      ],
+    );
+  }
+
+  //Add _handlePopPage
+  bool _handlePopPage(Route<dynamic> route, result) {
+    if (!route.didPop(result)) {
+      return false;
+    }
+    // TODO: Handle Onboarding and splash
+    // TODO: Handle state when user closes grocery item screen
+    // TODO: Handle state when user closes profile screen
+    // TODO: Handle state when user closes WebView screen
+    // 6
+    return true;
+  }
+
+  @override
+  Future<void> setNewRoutePath(configuration) async => null;
+}
